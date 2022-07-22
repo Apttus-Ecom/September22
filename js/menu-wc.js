9d3d6de3c7c0b373fd239faf060769fa81e561e7
divinabopanna <dboppana@apttus.com>
2022-07-22 13:26:39 +0530

    September'22 sdk documentation

--- js/menu-wc.js
+++ js/menu-wc.js
@@ -15,7 +15,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
         <nav>
             <ul class="list">
                 <li class="title">
-                    <a href="index.html" data-type="index-link">Conga Digital Commerce May &#x27;22</a>
+                    <a href="index.html" data-type="index-link">Conga Digital Commerce September &#x27;22</a>
                 </li>
 
                 <li class="divider"></li>
